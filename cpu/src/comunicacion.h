b0ea00a774a7180faa6496ba1878a64f81df35c6
axcaputo <acaputo@frba.utn.edu.ar>
2024-04-06 19:51:06 +0000

    Comienzo de las comunicaciones

---
+++ cpu/src/comunicacion.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_COMUNICACION_H_
+#define UTILS_COMUNICACION_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <utils/shared.h>
+
+
+#endif
--- utils/src/utils/shared.h
+++ utils/src/utils/shared.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "utilsServer.h"
+#include "utilsClient.h"
 
 /**
 * @fn    decir_hola
