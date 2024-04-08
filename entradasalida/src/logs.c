ec2ea077a2b137ef0886bf1726ca30c4494342da
marrkbau <bmarquez@frba.utn.edu.ar>
2024-04-08 04:13:27 +0000

    Borrando boludeces de logs.c

--- entradasalida/src/logs.c
+++ entradasalida/src/logs.c
@@ -101,24 +101,3 @@ void logEscribirArchivo(uint32_t pid, char* nombreArchivo, uint32_t tamanioAEscr
 
 
 
-
-# Obtener la ubicación del script
-script_dir=$(dirname "$0")
-
-# Directorio relativo al script
-directorio=$(realpath "$0")
-directorioEjecutable=$(dirname "$directorio")
-directorioModulo=$(dirname "$directorioEjecutable")
-directorioTp=$(dirname "$directorioModulo")
-
-test="$1"
-
-directorioConfig="$directorioTp/pruebas/configs/$test"
-
-directorioBin="$directorioModulo/bin/"
-
-
-# Cambiar al directorio
-cd "$directorioBin"
-# Ejecutar el programa con parámetros determinados
-./entradasalida "$directorioConfig/entradasalida.config" 
